31cdb5f50cee7ef6706b298a39ec08137b13c9a9
white-hun <121800818+white-hun@users.noreply.github.com>
2023-07-12 22:13:29 +0900

    Update Mock commentary

--- unit-basic/src/product/product_client.js
+++ unit-basic/src/product/product_client.js
@@ -1,7 +1,7 @@
 class ProductClient {
   fetchItems() {
     return fetch("http://example.com/login/id+password").then((response) => response.json());
-  }
+  } // 서버에서 데이터를 받아와 json으로 바꿔주는 임의의 코드 예시
 }
 
 module.exports = ProductClient;
--- unit-basic/src/product/test/product_service_no_di.test.js
+++ unit-basic/src/product/test/product_service_no_di.test.js
@@ -1,7 +1,9 @@
+// 나쁜 예제(mock을 남용하는 사례)
 const ProductService = require("../product_service_no_di");
 const ProductClient = require("../product_client");
-jest.mock("../product_client");
+jest.mock("../product_client"); // ProductClient는 mock을 사용한다(가짜 데이터)
 
+// 테스트하고자 하는 것: ProductService
 describe("ProductService", () => {
   const fetchItems = jest.fn(async () => [
     { item: "🥛", available: true },
@@ -32,3 +34,12 @@ describe("ProductService", () => {
     expect(fetchItems).toHaveBeenCalledTimes(1);
   });
 });
+
+// 단위테스트는 서로 모듈간의 상호작용을 테스트하면 안 되고
+// 단위 하나만 테스트 해야한다
+// ProductService 안에서 ProductClient의 fetchItems함수도 모르게 내부적으로 사용할 수 있다
+// 혹시나 ProductClient에서 환경적인 요인(함수가 실패하거나 데이터를 받아오지못하거나 또는 네트워크에 일시적으로 문제)이 생긴다면
+// ProductService 테스트 코드는 당연히 실패하게 된다
+// 이렇게 네트워크 상태에 의존하는 코드는 좋지 않다
+// ProductClient와 별개로 독립적으로 의존하지 않도록 작성하기 위해서는
+// ProductClient 자체를 mock하면된다
